e35ebdae8cd71f9b2a0916fd49d306ca67c5c8d9
ThanhTam <20521866@gm.uit.edu.vn>
2023-01-06 21:39:16 +0700

    Main-SignIn-Signup chưa Firebase
    Main-SignIn-Signup

---
+++ app/src/main/java/com/example/mobileappdev_nt118n11/SignUp.java
@@ -0,0 +1,14 @@
+package com.example.mobileappdev_nt118n11;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignUp extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
