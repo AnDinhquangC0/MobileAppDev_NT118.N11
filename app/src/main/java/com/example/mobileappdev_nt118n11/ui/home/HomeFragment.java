53158697aa3ed230ddda3c6f2e1e6b545ebee3e3
AnhTr14 <20520137@gm.uit.edu.vn>
2023-01-07 21:35:39 +0700

    Navigation

---
+++ app/src/main/java/com/example/mobileappdev_nt118n11/ui/home/HomeFragment.java
@@ -0,0 +1,28 @@
+package com.example.mobileappdev_nt118n11.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.mobileappdev_nt118n11.R;
+import com.example.mobileappdev_nt118n11.databinding.FragmentHomeBinding;
+
+public class HomeFragment extends Fragment {
+
+    private FragmentHomeBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+
+        return root;
+    }
+
+}
