4ba83cd434e24dc263092e9f5e7a77e30c63285f
ThanhTam <20521866@gm.uit.edu.vn>
2023-01-09 19:00:31 +0700

    Sửa SignUp

--- app/src/main/java/com/example/mobileappdev_nt118n11/SignUp.java
+++ app/src/main/java/com/example/mobileappdev_nt118n11/SignUp.java
@@ -20,7 +20,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 public class SignUp extends AppCompatActivity {
-    EditText edtPhone, edtName, edtPassword;
+    EditText edtPhone, edtName, edtPassword,edtGmail,edtAddress;
     Button btnSignUp;
     TextView txtSignIn;
     @Override
@@ -32,6 +32,8 @@ public class SignUp extends AppCompatActivity {
         edtName=(EditText) findViewById(R.id.edt_name_su);
         edtPassword=(EditText) findViewById(R.id.edt_password_su);
         edtPhone=(EditText) findViewById(R.id.edt_phone_su);
+        edtGmail=(EditText) findViewById(R.id.edt_gmail_su);
+        edtAddress=(EditText) findViewById(R.id.edt_address_su);
         btnSignUp=(Button) findViewById(R.id.btn_Sign_Up);
         txtSignIn=(TextView)findViewById(R.id.txt_sign_in);
 
@@ -56,15 +58,15 @@ public class SignUp extends AppCompatActivity {
                         {
                             mDialog.dismiss();
                             Toast.makeText(SignUp.this, "Phone Number already register !", Toast.LENGTH_SHORT).show();
-                            Intent signIn = new Intent(SignUp.this,SignIn.class);
-                            startActivity(signIn);
                         }
                         else
                         {
                             mDialog.dismiss();
-                            User user =new User(edtName.getText().toString(),edtPassword.getText().toString());
+                            User user =new User(edtName.getText().toString(),edtPassword.getText().toString(),edtGmail.getText().toString(),edtAddress.getText().toString());
                             table_user.child(edtPhone.getText().toString()).setValue(user);
                             Toast.makeText(SignUp.this, "Sign Up Successfully !", Toast.LENGTH_SHORT).show();
+                            Intent signIn = new Intent(SignUp.this,SignIn.class);
+                            startActivity(signIn);
                             finish();
                         }
 
