8e64a052f8d2e6b31fb3c042c6591b1b76b1927e
AnhTr14 <20520137@gm.uit.edu.vn>
2023-01-13 13:10:56 +0700

    Edit layout, read data to Profile
    Layout: SignIn, SignUp, Profile
    Read data to profile but not receive key

--- app/src/main/java/com/example/mobileappdev_nt118n11/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/mobileappdev_nt118n11/ui/profile/ProfileFragment.java
@@ -8,19 +8,53 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.example.mobileappdev_nt118n11.Model.User;
 import com.example.mobileappdev_nt118n11.R;
 import com.example.mobileappdev_nt118n11.databinding.FragmentHomeBinding;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class ProfileFragment extends Fragment {
 
     private FragmentHomeBinding binding;
+    private String key;
+    private FirebaseDatabase database;
+    private TextView tvName, tvPhone, tvEmail, tvAddress;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
         View root = inflater.inflate(R.layout.fragment_profile, container, false);
+        init(root);
 
+        key = "20520137";
+        database = FirebaseDatabase.getInstance();
+        database.getReference().child("User").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User user = snapshot.child(key).getValue(User.class);
+                tvName.setText(user.getName());
+                tvEmail.setText(user.getGmail());
+                tvAddress.setText(user.getAddress());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
         return root;
+    }
+
+    public void init(View root) {
+        tvName = root.findViewById(R.id.tv_profile_name);
+        tvPhone = root.findViewById(R.id.tv_profile_phone);
+        tvEmail = root.findViewById(R.id.tv_profile_email);
+        tvAddress = root.findViewById(R.id.tv_profile_address);
     }
 }
