0a6cf31305f7d11c8f57a61bf0b2bca51eaa74da
AnhTr14 <20520137@gm.uit.edu.vn>
2023-01-13 22:20:32 +0700

    Navigation Admin

---
+++ app/src/main/java/com/example/mobileappdev_nt118n11/ui/MenuFragment.java
@@ -0,0 +1,24 @@
+package com.example.mobileappdev_nt118n11.ui;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.mobileappdev_nt118n11.R;
+
+
+public class MenuFragment extends Fragment {
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        View root = inflater.inflate(R.layout.fragment_menu, container, false);
+
+        return root;
+    }
+
+}
---
+++ app/src/main/java/com/example/mobileappdev_nt118n11/ui/TypeFragment.java
@@ -0,0 +1,23 @@
+package com.example.mobileappdev_nt118n11.ui;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.mobileappdev_nt118n11.R;
+
+public class TypeFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        View root = inflater.inflate(R.layout.fragment_type, container, false);
+
+        return root;
+    }
+}
