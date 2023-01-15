a8b10ce225135d161c520f743e5532fb3e8ce56e
AnDinhquangC0 <20520370@gm.uit.edu.vn>
2023-01-15 17:38:56 +0700

    update search layout

--- app/src/main/java/com/example/mobileappdev_nt118n11/Interface/ItemClickListener.java
+++
@@ -1,7 +0,0 @@
-package com.example.mobileappdev_nt118n11.Interface;
-
-import android.view.View;
-
-public interface ItemClickListener {
-    Void onClick(View view, int position, boolean isLongClick);
-}
