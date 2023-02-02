5be0681dcb39fa75d62bff88fcf0edc8c377e479
HoThiThanhTam <104922541+HoThiThanhTam@users.noreply.github.com>
2023-02-02 21:17:31 +0700

    Update Profile

--- app/src/main/java/com/example/mobileappdev_nt118n11/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/mobileappdev_nt118n11/ui/profile/ProfileFragment.java
@@ -8,10 +8,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.mobileappdev_nt118n11.Model.User;
 import com.example.mobileappdev_nt118n11.R;
+import com.example.mobileappdev_nt118n11.SignIn;
 import com.example.mobileappdev_nt118n11.databinding.FragmentHomeBinding;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -24,13 +28,15 @@ public class ProfileFragment extends Fragment {
     private FragmentHomeBinding binding;
     private String key;
     private FirebaseDatabase database;
-    private TextView tvName, tvPhone, tvEmail, tvAddress;
-
+    private TextView tvPhone;
+    private EditText edName, edEmail,edAddress;
+    private Button btnUpdate;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
         View root = inflater.inflate(R.layout.fragment_profile, container, false);
         init(root);
+
 
         key = Phone.Key_Phone;
         database = FirebaseDatabase.getInstance();
@@ -39,9 +45,9 @@ public class ProfileFragment extends Fragment {
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 User user = snapshot.child(key).getValue(User.class);
                 tvPhone.setText(key);
-                tvName.setText(user.getName());
-                tvEmail.setText(user.getGmail());
-                tvAddress.setText(user.getAddress());
+                edName.setText(user.getName());
+                edEmail.setText(user.getGmail());
+                edAddress.setText(user.getAddress());
             }
 
             @Override
@@ -49,13 +55,23 @@ public class ProfileFragment extends Fragment {
 
             }
         });
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                database.getReference().child("User").child(key).child("Name").setValue(edName.getText().toString());
+                database.getReference().child("User").child(key).child("Address").setValue(edAddress.getText().toString());
+                database.getReference().child("User").child(key).child("Gmail").setValue(edEmail.getText().toString());
+
+            }
+        });
         return root;
     }
 
     public void init(View root) {
-        tvName = root.findViewById(R.id.tv_profile_name);
+        edName = root.findViewById(R.id.tv_profile_name);
         tvPhone = root.findViewById(R.id.tv_profile_phone);
-        tvEmail = root.findViewById(R.id.tv_profile_email);
-        tvAddress = root.findViewById(R.id.tv_profile_address);
+        edEmail = root.findViewById(R.id.tv_profile_email);
+        edAddress = root.findViewById(R.id.tv_profile_address);
+        btnUpdate =root.findViewById(R.id.btn_profile_Edit);
     }
 }
