e8462cf8e303f1e6a88cc111d5a200df8591b6ae
AnhTr14 <20520137@gm.uit.edu.vn>
2023-02-09 21:05:40 +0700

    Logout

--- app/src/main/java/com/example/mobileappdev_nt118n11/NavigationActivity.java
+++ app/src/main/java/com/example/mobileappdev_nt118n11/NavigationActivity.java
@@ -70,7 +70,7 @@ public class NavigationActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_favourite, R.id.nav_cart, R.id.nav_profile)
+                R.id.nav_home, R.id.nav_favourite, R.id.nav_cart, R.id.nav_profile, R.id.nav_logout)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_navigation);
